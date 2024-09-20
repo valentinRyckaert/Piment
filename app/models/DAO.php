cb3b9903d27618a2ae3f5765d0b6baed6add66a9
E.Villette <e.villette@eleve.leschartreux.net>
2024-09-20 14:52:26 +0200

    DAO intro

---
+++ app/models/DAO.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace piment\models; 
+
+abstract class DAO {
+    /* @var $cnx <PDO> */
+    protected $cnx;
+
+    public function __construct($cnx) {
+        //TODO
+    }
+    public abstract function find($id) ;
+
+    public abstract function remove($model): bool; 
+    public abstract function save($model): bool;
+    public abstract function update($model): bool;
+    public abstract function findAll(int $offset = 0, int $limit = 1024): array ;
+    public abstract function count(): int ;
+}
