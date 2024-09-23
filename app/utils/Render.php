2255374418b4e696c7b14045ace431f7c2a73524
E.Villette <e.villette@eleve.leschartreux.net>
2024-09-23 16:55:25 +0200

    new fill : Render and ShowData

---
+++ app/utils/Render.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace piment\utils;
+
+class Render
+{
+    $path = ""; // TODO cf __DIR__
+    
+    if(SinglletonconfigReader::getInstance()->getValue("debug") == "true"){
+        set_error_handler('self::error_handler');
+    }
+    public static render(string $page, array $data=null) : ?string {
+
+        ob_start();
+        if($data==null) {
+            extract($data);
+        }
+        include_once $path;
+        
+        $content = ob_get_contents();
+        ob_clean();
+        return $content;
+    }
+
+    private static function  errror_handler($severity, $message, $filename, $linenum) {
+        if(errror_reporting() == 0) {
+            return;
+        }
+    }
+}
---
+++ app/views/ShowData.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>ShowData</title>
+  </head>
+  <body>
+    <div>nom : <?php echo $nom ?></div>
+  </body>
+</html>
