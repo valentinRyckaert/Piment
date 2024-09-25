30bbdfb52ee02adc3222b37f12e91673bbe9ca72
Valentin Ryckaert <v.ryckaert@eleve.leschartreux.net>
2024-09-25 08:40:13 +0200

    update render

--- app/utils/Render.php
+++ app/utils/Render.php
@@ -2,14 +2,14 @@
 
 namespace piment\utils;
 
-class Render
-{
-    $path = ""; // TODO cf __DIR__
-    
-    if (SinglletonconfigReader::getInstance()->getValue("debug") == "true"){
-        set_error_handler('self::error_handler');
-    }
-    public static render(string $page, array $data=null) : ?string {
+class Render {
+    private $path = __DIR__ . "/views";
+
+    public static function render(string $page, array $data=null) : ?string {
+
+        if (SingletonconfigReader::getInstance()->getValue("debug") == "true"){
+            set_error_handler(self::errror_handler());
+        }
 
         ob_start();
         if($data==null) {
