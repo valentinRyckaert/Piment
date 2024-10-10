8a396b4f7ab90387b34d8b7965f22817b9aa33a2
E.Villette <e.villette@eleve.leschartreux.net>
2024-10-10 22:03:33 +0200

    PhoneValidator.php with this unit test

--- app/utils/validators/PhoneValidator.php
+++ app/utils/validators/PhoneValidator.php
@@ -10,7 +10,8 @@ class PhoneValidator implements InterfaceValidator
      */
      public function validate(string $data): bool
      {
-         // TODO: Implement validate() method.
+            // règle de validation du numéro de téléphone : 10 chiffres, commençant par 0, accepte les éspace et les tirets entre deux par paire.
+            return preg_match("/^0[1-9]([-. ]?[0-9]{2}){4}$/", $data);
 
      }
 }
