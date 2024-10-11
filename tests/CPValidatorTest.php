2093c6eb0bf6233f69405f03d3f7e0db50d1b5e6
E.Villette <e.villette@eleve.leschartreux.net>
2024-10-11 13:09:41 +0200

    upgrade CPValidatorTest.php

--- tests/CPValidatorTest.php
+++ tests/CPValidatorTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace utils\validators;
+namespace tests;
 
-use piment\utils\validators\CPValidator;
 use PHPUnit\Framework\TestCase;
+use piment\utils\validators\CPValidator;
 
 class CPValidatorTest extends TestCase
 {
@@ -14,12 +14,43 @@ class CPValidatorTest extends TestCase
         $this->assertTrue($cpValidator->validate("75000"));
         $this->assertTrue($cpValidator->validate("75001"));
         $this->assertTrue($cpValidator->validate("75002"));
+    }
 
+    public function testTooShort()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate("7500")); // Trop court
+    }
+
+    public function testTooLong()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate("750000")); // Trop long
+    }
+
+    public function testNonNumeric()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate('abcd1')); // Non numérique
+    }
+
+    public function testAnotherTooShort()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate('1234')); // Trop court
+    }
+
+    public function testContainsLetters()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate('7500a')); // Contient des lettres
+    }
+
+    public function testOutOfRange()
+    {
+        $cpValidator = new CPValidator();
         $this->assertFalse($cpValidator->validate('99000')); // Hors plage
     }
+
+
 }
