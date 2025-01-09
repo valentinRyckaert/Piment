c0571c3828e8e721e8b2e6d49aad93fcb06552fe
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-01-09 22:39:10 +0100

    fix index.php

--- html/index.php
+++ html/index.php
@@ -5,6 +5,7 @@ require_once '../vendor/autoload.php';
 use piment\controllers\CaserneController;
 use piment\controllers\DefaultController;
 use piment\controllers\PompierController;
+use piment\controllers\UserController;
 use piment\utils\Auth;
 use piment\utils\Router;
 
@@ -38,4 +39,10 @@ Router::get('/pompier/delete/#', new PompierController(), 'delete', Auth::$CANDE
 Router::post('/caserne/delete', new CaserneController(), 'do_delete', Auth::$CANDELETECASERNE);
 Router::post('/pompier/delete', new PompierController(), 'do_delete', Auth::$CANDELETEPOMPIER);
 
-Router::get('?', new DefaultController(), 'not_found_404');
+// User
+Router::get('/user/show', new UserController(), 'show', Auth::$CANREADUSER);
+Router::get('/user/detail/#', new UserController(), 'detail', Auth::$CANREADUSER);
+Router::get('/user/add', new UserController(), 'create', Auth::$CANCREATEUSER);
+Router::post('/user/save', new UserController(), 'do_create', Auth::$CANCREATEUSER);
+Router::get('/user/delete/#', new UserController(), 'delete', Auth::$CANDELETEUSER);
+Router::post('/user/delete', new UserController(), 'do_delete', Auth::$CANDELETEUSER);
